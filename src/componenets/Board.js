0a5e3f601649a82751bf21a69f873a552ef14014
MaXsTyLe228 <kashlyatsky@gmail.com>
2022-06-03 13:36:23 +0300

    env

---
+++ src/componenets/Board.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {useDispatch} from "react-redux";
+
+const Board = () => {
+    const dispatch = useDispatch()
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Board;
